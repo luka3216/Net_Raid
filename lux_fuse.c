a8aa0f9bcf351378d17a7c95e046c9a47a083083
luka3216 <lkiri15@freeuni.edu.ge>
2018-08-01 14:25:31 +0400

    removed excess funcs and vars

--- lux_fuse.c
+++ lux_fuse.c
@@ -9,32 +9,6 @@
 #include "lux_fuse.h"
 #include "lux_common.h"
 
-static const char *hello_str = "Hello World!\n";
-static const char *hello_path = "/hello";
-
-static int hello_getattr(const char *path, struct stat *stbuf)
-{
-  int res = 0;
-
-  memset(stbuf, 0, sizeof(struct stat));
-  if (strcmp(path, "/") == 0)
-  {
-    stbuf->st_mode = __S_IFDIR | 0755;
-    stbuf->st_nlink = 2;
-  }
-  else if (strcmp(path, hello_path) == 0)
-  {
-    stbuf->st_mode = __S_IFREG | 0444;
-    stbuf->st_nlink = 1;
-    stbuf->st_size = strlen(hello_str);
-  }
-  else
-    res = -ENOENT;
-
-  printf("%s %d\n", path, res);
-
-  return res;
-}
 
 int get_live_server()
 {
@@ -158,7 +132,7 @@ static int lux_read(const char *path, char *buf, size_t size, off_t offset,
 
   int server_id = get_live_server();
   
-  printf("attemting (read) contact with server %d for path %s for %d bytes.\n", server_id, path, size);
+  printf("attemting (read) contact with server %d for path %s for %zu bytes.\n", server_id, path, size);
   fflush(stdout);
 
   int sent = send(_this_storage.servers[server_id]->socket_fd, &input, sizeof(struct raid_one_input), 0);
@@ -186,51 +160,6 @@ static int lux_read(const char *path, char *buf, size_t size, off_t offset,
 
 }
 
-static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-                         off_t offset, struct fuse_file_info *fi)
-{
-  (void)offset;
-  (void)fi;
-
-  if (strcmp(path, "/") != 0)
-    return -ENOENT;
-
-  filler(buf, ".", NULL, 0);
-  filler(buf, "..", NULL, 0);
-  filler(buf, hello_path + 1, NULL, 0);
-
-  return 0;
-}
-
-static int hello_open(const char *path, struct fuse_file_info *fi)
-{
-  if (strcmp(path, hello_path) != 0)
-    return -ENOENT;
-
-  if ((fi->flags & 3) != O_RDONLY)
-    return -EACCES;
-
-  return 0;
-}
-
-static int hello_read(const char *path, char *buf, size_t size, off_t offset,
-                      struct fuse_file_info *fi)
-{
-  size_t len;
-  (void)fi;
-
-  len = strlen(hello_str);
-  if (offset < len)
-  {
-    if (offset + size > len)
-      size = len - offset;
-    memcpy(buf, hello_str + offset, size);
-  }
-  else
-    size = 0;
-
-  return size;
-}
 
 static struct fuse_operations hello_oper = {
     .getattr = lux_getattr,
